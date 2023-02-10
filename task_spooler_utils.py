ae8db6960419f40c578434a4672d6d8a2f894265
ric <ric@unitxlabs.com>
2023-02-10 08:54:24 -0800

    Changes to support vanila task spooler

--- task_spooler_utils.py
+++ task_spooler_utils.py
@@ -1,10 +1,10 @@
 import glob
-import json
 import os
 import subprocess
 import pandas as pd
+from io import StringIO
 
-TASK_SPOOLER_CMD = "ts"
+TASK_SPOOLER_CMD = "tsp"
 
 
 def list_jobs(socket_name):
@@ -13,15 +13,46 @@ def list_jobs(socket_name):
     """
     env = get_env(socket_name)
     output = subprocess.check_output(
-        f"{TASK_SPOOLER_CMD} -l -M json", env=env, shell=True, encoding="utf-8"
+        f"{TASK_SPOOLER_CMD} -l", env=env, shell=True, encoding="utf-8"
     )
-    data = json.loads(output)
-    df = pd.DataFrame(data=data)
+
+    return parse_list_string(output)
+
+def parse_list_string(list_string):
+    """
+    """
+    lines = list_string.split("\n")
+    data = []
+    for line in lines[1:]:
+        tokens = line.split()
+        if len(tokens) == 0:
+            break
+        print(tokens)
+        job_id = tokens[0]
+        state = tokens[1]
+        output = tokens[2]
+        try:
+            e_level = str(int(tokens[3]))
+            i = 4
+        except ValueError:
+            e_level = ""
+            i = 3
+        try:
+            time_ms = str(float(tokens[i].split("/")[0]) * 1000)
+            i = i+1
+        except ValueError:
+            time_ms = ""
+        command = " ".join(tokens[i:])
+        job_info = [job_id, state, output, e_level, time_ms, command]
+        print(job_info)
+        data.append(job_info)
+    print(data)
+    df = pd.DataFrame(data, columns=["ID", "State", "Output", "E-Level", "Time_ms", "Command"])
     df = df.set_index("ID", drop=False).sort_index()
     return df
 
 
-def remove(job_id, socket_name):
+def tsp_remove(job_id, socket_name):
     """
     Remove a task-spooler job.
     """
@@ -40,7 +71,7 @@ def remove(job_id, socket_name):
     return proc
 
 
-def kill(job_id, socket_name):
+def tsp_kill(job_id, socket_name):
     """
     Kill a running task-spooler job.
     """
